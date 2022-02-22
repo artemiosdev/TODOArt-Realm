6d123a7b9d1cf708e6a31e3562ee08d94876f5be
artemiosdev <flyboroda@gmail.com>
2022-02-22 16:55:23 +0300

    Add TableView Delegate Methods and Accessories

--- TODOArt/ToDoListViewController.swift
+++ TODOArt/ToDoListViewController.swift
@@ -23,10 +23,27 @@ class ToDoListViewController: UITableViewController {
         
       let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
 //      or
-//        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+//      let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
         
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
 }
+
+
 
