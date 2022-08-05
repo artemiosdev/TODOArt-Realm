e3208e25e47fd6f2d745016d78b0c4045eec471b
artemiosdev <flyboroda@gmail.com>
2022-08-05 15:30:16 +0300

    Creating a Custom Data Model. Fix bugs in tableView.dequeueReusableCell and Table View cells. tableView.reloadData()

--- TODOArt/Controllers/ToDoListViewController.swift
+++ TODOArt/Controllers/ToDoListViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-    var itemArray = ["Example 1", "Example 2", "Example 3"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let item = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = item
-        }
+        
+        let newItem = Item()
+        newItem.title = "Example 1"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Example 2"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Example 3"
+        itemArray.append(newItem3)
+        
+//        if let item = defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = item
+//        }
         
     }
     
@@ -28,22 +41,38 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
       let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-//      or
+// или тоже самое
 //      let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        let item = itemArray[indexPath.row]
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+// или тоже самое
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
         return cell
     }
     
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+// или тоже самое с if
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+        
+        tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -56,7 +85,11 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new TODOArt Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the Add Item button on our UIAlert
-            self.itemArray.append(textField.text ?? "New item")
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ TODOArt/Data Model/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  TODOArt
+//
+//  Created by Artem Androsenko on 05.08.2022.
+//
+
+import Foundation
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
