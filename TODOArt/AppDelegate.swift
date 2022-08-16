c87ffd5e6511a6c949373b6139911bd8763dc76f
artemiosdev <flyboroda@gmail.com>
2022-08-16 23:24:53 +0300

    Introduction to Realm. Add Realm file for save Data

--- TODOArt/AppDelegate.swift
+++ TODOArt/AppDelegate.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // The location of the Realm file
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Artem"
+        data.age = 28
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
+        
         return true
     }
 
